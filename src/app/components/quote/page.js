ea136569c40c5eb97dc12c8266d780a913fc1e2d
Nirav <niravpatel5481@gmail.com>
2024-12-17 19:57:27 +0530

    first commit

---
+++ src/app/components/quote/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../../css/Quote.module.css'
+
+const Page = () => {
+    return (
+        <div>
+            <div className={styles.quote} style={{fontSize:"40px"}}>
+                "There's no glory in being ordinary<br /> Dare to be different"
+            </div>
+        </div>
+    )
+}
+
+export default Page
